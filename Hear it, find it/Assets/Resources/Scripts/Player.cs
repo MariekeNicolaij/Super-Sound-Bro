89b44c6679fb90d9469273b9f1041f30ab809ee2
Marieke <mariekenicolay@hotmail.com>
2018-03-20 14:16:22 +0100

    Added folder structure, background, player and a timer delay for shooting

---
+++ Hear it, find it/Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Sprite laserSprite;         // It points to cursor / right-joystick
+    float laserDelay;
+    float maxLaserDelay = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Inputs();
+    }
+
+    void Inputs()
+    {
+        if (Input.GetButtonDown("Fire"))
+        
+            Debug.Log(CanFire());
+         if (CanFire()&&Input.GetButtonDown("Fire"))
+            Fire();
+    }
+
+    bool CanFire()
+    {
+        if (laserDelay > 0)
+        {
+            laserDelay -= Time.deltaTime;
+            return false;
+        }
+        else
+            return true;
+    }
+
+    void Fire()
+    {
+        laserDelay = maxLaserDelay;
+    }
+
+    Vector3 CursorDirection()
+    {
+        return Vector3.zero;
+    }
+}
