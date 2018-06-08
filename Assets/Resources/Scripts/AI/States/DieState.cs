2a95c6d300812bf0fa70db175e7e8172f6fc0311
Mariekeletje <mariekenicolay@hotmail.com>
2018-06-08 15:28:17 +0200

    Enemy rotates and shrinks when dying

--- Assets/Resources/Scripts/AI/States/DieState.cs
+++ Assets/Resources/Scripts/AI/States/DieState.cs
@@ -10,13 +10,18 @@ public class DieState : IState
     public void Enter(BaseAI owner)
     {
         owner.dieParticleSystem.Play();
+        GameObject.Destroy(owner.rBody);
+        GameObject.Destroy(owner.GetComponent<BoxCollider2D>());
         owner.Invoke("StartDieAnimation", dieAnimationTime);
     }
 
     // Update
     public void Execute(BaseAI owner)
     {
-
+        // Rotate enemy
+        owner.transform.Rotate(Vector3.forward * Time.deltaTime * 500); // 500 = speed
+        // Shrink enemy
+        owner.transform.localScale = Vector3.Lerp(owner.transform.localScale, Vector3.zero , Time.deltaTime / dieAnimationTime);
     }
 
     public void Exit(BaseAI owner)
